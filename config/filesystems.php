a2c058a21c2f06dadcd4e38739088b725338da22
yogabudisantoso <yextiuxiu@gmail.com>
2026-08-02 13:51:52 +0700

    Clean up unused workflow files and trigger deployment

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => base_path('public/storage'),
             'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
